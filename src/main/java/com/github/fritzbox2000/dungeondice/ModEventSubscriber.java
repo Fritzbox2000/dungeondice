ea557d5860b11372f0358e51d2f0c31ddf0ce47a
Fritzbox2000 <woonters@gmail.com>
2020-02-24 12:15:45 +0000

    Add the first item and general stuff

--- src/main/java/com/github/fritzbox2000/dungeondice/ModEventSubscriber.java
+++ src/main/java/com/github/fritzbox2000/dungeondice/ModEventSubscriber.java
@@ -1,18 +1,19 @@
 package com.github.fritzbox2000.dungeondice;
 
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.registries.IForgeRegistryEntry;
+import com.github.fritzbox2000.dungeondice.init.ModItemGroups;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.registries.IForgeRegistryEntry;
 
 @EventBusSubscriber(modid = DungeonDice.MODID, bus = EventBusSubscriber.Bus.MOD)
 public final class ModEventSubscriber {
 
     @SubscribeEvent
     public static void onRegisterItems(RegistryEvent.Register<Item> event) {
-        event.getRegistry().registerAll(setup(new Item(new Item.Properties()),"example_item"));
+        event.getRegistry().registerAll(setup(new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)),"example_item"));
     }
 
     public static <T extends IForgeRegistryEntry<T>> T setup(final T entry, final String name) {
---
+++ src/main/java/com/github/fritzbox2000/dungeondice/init/ModItems.java
@@ -0,0 +1,11 @@
+package com.github.fritzbox2000.dungeondice.init;
+
+import com.github.fritzbox2000.dungeondice.DungeonDice;
+import net.minecraft.item.Item;
+import net.minecraftforge.registries.ObjectHolder;
+
+@ObjectHolder(DungeonDice.MODID)
+public class ModItems {
+    public static final Item EXAMPLE_ITEM = null;
+
+}
